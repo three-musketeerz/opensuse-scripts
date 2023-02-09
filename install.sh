55727b0d8811cb74f74ccaf9407204b1283f345f
Karina Vega <akun.karina.vega@gmail.com>
2023-02-09 21:16:01 +0700

    install: change project folder

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ ln -sf $(pwd)/git/pull.sh $HOME/bin/pull
 ln -sf $(pwd)/git/run-pull.sh $HOME/bin/run-pull
 ln -sf $(pwd)/dropbox/install.sh $HOME/bin/dropbox-install
 
-mkdir $HOME/.idx-repo
-git clone --depth=1 https://github.com/yiisoft/yii $HOME/.idx-repo/yii
+mkdir $HOME/.3m
+git clone --depth=1 https://github.com/yiisoft/yii $HOME/.3m/yii
--- remove.sh
+++ remove.sh
@@ -4,4 +4,4 @@ rm $HOME/bin/pull
 rm $HOME/bin/run-pull
 rm $HOME/bin/dropbox-install
 
-rm -rf $HOME/.idx-repo
+rm -rf $HOME/.3m
