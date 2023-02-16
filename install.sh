1e9e3227612941114c9b9f5c585e134b560e9996
Joko Putranto <akun.joko.putranto@gmail.com>
2023-02-16 12:44:53 +0700

    installer fix add path to .profile

--- install.sh
+++ install.sh
@@ -3,19 +3,18 @@ ENV3M="$HOME/.3m"
 mkdir -p $ENV3M
 
 ENVSCR="$ENV3M/scripts"
-if [ ! -z "${ENVSCR}" ]
+if [ ! -d "${ENVSCR}" ]
 then
  git clone --depth=1 https://github.com/three-musketeerz/opensuse-scripts $ENVSCR
 else
  cd "${ENVSCR}"; git pull
 fi
 
-ENVBIN="$ENV3m/bin"
+ENVBIN="$ENV3M/bin"
 mkdir -p $ENVBIN
-rm $ENVBIN/*
 
 ln -sf $ENVSCR/git/pull.sh $ENVBIN/git-pull
 ln -sf $ENVSCR/git/pulls.sh $ENVBIN/git-pulls
 ln -sf $ENVSCR/dropbox/install.sh $ENVBIN/dropbox-install
 
-export PATH="$ENVBIN:$PATH"
+echo "export PATH=${ENVBIN}:\${PATH}" >> $HOME/.profile
